ba015a4a6bf8d967e48046543790fe6f66e6e35d
Zander Ambrose <alexander.d.ambrose@gmail.com>
2021-06-14 10:38:31 -0400

    added error handling to the api call so that an error will display on the screen if no request is found

--- src/Components/Pantry/ManipulateData.js
+++ src/Components/Pantry/ManipulateData.js
@@ -9,23 +9,42 @@ function DisplayData(props) {
     useEffect(() => {
         let queryString = string.trim().replace(/,/gi, '').split(' ').join(',+')
         const fetchProducts = async () => {
-            const fetchData = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=4e345a393bbd462182c89705d3914f24&ingredients=${queryString}&number=4`)
-            const dataArr = fetchData.data
-            setData(dataArr)
+            try {
+                const fetchData = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=4e345a393bbd462182c89705d3914f24&ingredients=${queryString}&number=4`)
+                if (fetchData.data.length !== 0) {
+                    const dataArr = await fetchData.data
+                    setData(dataArr)
+                } else {
+                    const dataArr = ["No data found.  Please try again."]
+                    setData(dataArr)
+                }
+            }
+            catch (error) {
+                console.log("catch statement", error)
+            }
         }
         fetchProducts()
     }, [string])
 
     return (
         <div>
-            {console.log(data)}
-            {data.map((item => {
-                return (
-                    <div key={item.id} className="displayData">
-                        <h1>{item.title}</h1>
-                        <img src={item.image} alt="displayed food images" />
-                    </div>
-                )
+            {data.map(((item, idx) => {
+                if (data.length > 1) {
+                    return (
+                        <div key={item.id} className="displayContainer">
+                            <div className="displayData">
+                                <h1>{item.title}</h1>
+                                <img src={item.image} alt="displayed food images" />
+                            </div>
+                        </div>
+                    )
+                } else {
+                    return (
+                        <div key={idx}>
+                            <h1>{data[0]}</h1>
+                        </div>
+                    )
+                }
             }))}
         </div>
     )
